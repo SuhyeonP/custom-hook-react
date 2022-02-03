86e809e1650d128fce234c5d768cfa49f972db84
suhyeonP <ahah12k@gmail.com>
2022-02-03 13:24:56 +0900

    :hammer: upgrade ver

--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
-import * as pluralize from 'pluralize';
-
-/**
- * @Method: Returns the plural form of any noun.
- * @Param: {string}
- * @Return: {string}
- */
-export function getPlural(str: any): string {
-    return pluralize.plural(str)
-}
-
 export { useInput } from './dist'
